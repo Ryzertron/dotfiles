c64bb72b3c8dd2c4c2af0d4b49e879845786bfb0
GOKULKRISHNA <107233184+Ryzertron@users.noreply.github.com>
2026-01-13 13:57:15 +0530

    Configured terminal to open in fullscreen on default

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,6 +1,14 @@
 -- Call wezterm API
 local wezterm = require("wezterm")
 
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function(cmd)
+	local _, _, window = mux.spawn_window(cmd or {})
+	-- The following line toggles fullscreen on the spawned window
+	window:gui_window():toggle_fullscreen()
+end)
+
 -- Object to hold the config
 local config = wezterm.config_builder()
 
@@ -13,9 +21,9 @@ config.enable_tab_bar = false
 config.term = "xterm-256color"
 config.color_scheme = "Rapture"
 config.window_padding = {
-	left = 2,
+	left = 10,
 	right = 2,
-	top = 0,
+	top = 15,
 	bottom = 0,
 }
 config.default_prog = { "tmux" }
