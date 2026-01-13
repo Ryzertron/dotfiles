fe1ea561bd1352b2ddd1f2cbcd4ef00a571709e4
GOKULKRISHNA <107233184+Ryzertron@users.noreply.github.com>
2026-01-13 12:46:17 +0530

    Changed nvim colorscheme to look like vscode dark

--- config/.config/nvim/lua/plugins/colorscheme.lua
+++ config/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,20 @@
 return {
   {
+    "Mofiqul/vscode.nvim",
+    lazy = true,
+    priority = 1000, -- make sure to load this before other plugins that change the colorscheme
+    opts = {
+      -- options go here
+      style = "dark", -- choose between "dark" or "light"
+      transparent = false, -- set to true if you want a transparent background
+      italic_comments = true, -- set to true to italicize comments
+    },
+  },
+  -- Configure LazyVim to use the vscode colorscheme
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night",
+      colorscheme = "vscode",
     },
   },
 }
